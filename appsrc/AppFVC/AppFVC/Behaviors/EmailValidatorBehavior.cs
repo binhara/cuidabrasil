0746ebf1bde52e9a17495283e030cf3b1d353de9
binhara <binhara@gmail.com>
2020-03-29 10:07:57 -0300

    Adicionados comportamentos de tela

---
+++ appsrc/AppFVC/AppFVC/Behaviors/EmailValidatorBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace AppFVC.Behaviors
+{
+    public class EmailValidatorBehavior : Behavior<Entry>
+    {
+        const string emailRegex = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+            @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += HandleTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        public bool ValidateEmail(string email)
+        {
+            bool IsValid = (Regex.IsMatch(email, emailRegex));
+            return IsValid;
+        }
+
+        void HandleTextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool IsValid = false;
+            IsValid = (Regex.IsMatch(e.NewTextValue, emailRegex, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250)));
+            ((Entry)sender).TextColor = IsValid ? Color.Default : Color.Red;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= HandleTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
+
---
+++ appsrc/AppFVC/AppFVC/Behaviors/NomeBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace AppFVC.Behaviors
+{
+    public class NomeBehavior : Behavior<Entry>
+    {
+        const string nomeRegex = @"^((\b[A-zÀ-ú']{2,40}\b)\s*){2,}$";
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+
+
+        void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool IsValid = false;
+            IsValid = (Regex.IsMatch(e.NewTextValue, nomeRegex, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250)));
+            ((Entry)sender).TextColor = IsValid ? Color.Default : Color.Red;
+        }
+    }
+}
