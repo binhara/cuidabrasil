23dd6f09bfdb11c6dd3d580df573b59b8d32f5ef
carolys <carolina_myasue@hotmail.com>
2020-03-29 01:28:30 -0300

    Layout da tela Welcome criado.

--- appsrc/AppFVC/AppFVC/App.xaml.cs
+++ appsrc/AppFVC/AppFVC/App.xaml.cs
@@ -23,13 +23,14 @@ namespace AppFVC
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            await NavigationService.NavigateAsync("WelcomePage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<WelcomePage, WelcomePageViewModel>();
         }
     }
 }
