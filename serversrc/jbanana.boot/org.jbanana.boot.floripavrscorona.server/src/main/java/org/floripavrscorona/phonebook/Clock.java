001241ace0cbdcb02b4142383d8877de1ac93235
Sandro Bihaiko <bihaiko@gmail.com>
2020-03-29 16:16:04 -0300

    segundo endpoint funcionando

---
+++ serversrc/jbanana.boot/org.jbanana.boot.floripavrscorona.server/src/main/java/org/floripavrscorona/phonebook/Clock.java
@@ -0,0 +1,17 @@
+package org.floripavrscorona.phonebook;
+
+import org.floripavrscorona.phonebook.bo.Phonebook;
+import org.jbanana.core.Container;
+import org.prevayler.Prevayler;
+
+public class Clock {
+	
+	public static long now() {
+		Container container = Container.getContainer(Phonebook.class);
+		Prevayler prevayler = container.getPrevayler();
+		if(prevayler==null) return 0;
+		
+		org.prevayler.Clock clock = prevayler.clock();
+		return clock.time().getTime();
+	}
+}
--- serversrc/jbanana.boot/org.jbanana.boot.floripavrscorona.server/src/main/java/org/floripavrscorona/phonebook/bo/Contact.java
+++ serversrc/jbanana.boot/org.jbanana.boot.floripavrscorona.server/src/main/java/org/floripavrscorona/phonebook/bo/Contact.java
@@ -3,18 +3,17 @@ package org.floripavrscorona.phonebook.bo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.floripavrscorona.phonebook.bo.CoronaStatus.Status;
 import org.jbanana.core.Persistent;
 import org.jbanana.rest.Restable;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @EqualsAndHashCode
-@NoArgsConstructor
 public class Contact implements Persistent, Restable{
 
 	private static final long serialVersionUID = 1L;
@@ -25,17 +24,23 @@ public class Contact implements Persistent, Restable{
 	@Getter @Setter private String phone; 
 	@Getter @Setter private String name;
 	@Getter @Setter private int age;
-//	@Getter private final List<CoronaStatus> journal = new ArrayList<>();
+	@Getter private final List<CoronaStatus> journal = new ArrayList<>();
 
 	@Override
 	public String getId() {
 		return phone;
 	}		
 	
+	public Contact(){
+		this.id = null;
+		this.journal.add(new CoronaStatus(Status.UNLOCKED));
+	}
+	
 	public Contact(String phone, String name, int age) {
 		this.phone = phone;
 		this.name = name;
 		this.age = age;
+		this.journal.add(new CoronaStatus(Status.UNLOCKED));
 	}
 	
 	@Override public String xpathContainer() {return "contacts";}
