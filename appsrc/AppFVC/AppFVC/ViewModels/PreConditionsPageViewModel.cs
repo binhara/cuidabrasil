255cda30723632d6578de6d703d3b9eaeb5db76b
Adriano D'lucca <adriano@azuris.com.br>
2020-04-02 11:21:50 -0300

    Botões sim e nao implementados na pagina PreConditions

--- appsrc/AppFVC/AppFVC/ViewModels/PreConditionsPageViewModel.cs
+++ appsrc/AppFVC/AppFVC/ViewModels/PreConditionsPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Navigation;
+using System;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -9,6 +10,8 @@ namespace AppFVC.ViewModels
     {
         private readonly INavigationService _navigationService;
         public Command NavegarNext { get; set; }
+        public Command SimCommand { get; set; }
+        public Command NaoCommand { get; set; }
         private bool _isBusy;
         public bool IsBusy
         {
@@ -18,11 +21,65 @@ namespace AppFVC.ViewModels
                 { SetProperty(ref _isBusy, value); }
             }
         }
+    
+        private string _simColor;
+
+        [Obsolete]
+        public string SimColor
+        {
+            get
+            {
+                return _simColor;
+            }
+            set
+            {
+                if (_simColor != value)
+                {
+                    _simColor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _naoColor;
+
+        [Obsolete]
+        public string NaoColor
+        {
+            get
+            {
+                return _naoColor;
+            }
+            set
+            {
+                if (_naoColor != value)
+                {
+                    _naoColor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+       
         public PreConditionsPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             IsBusy = false;
             _navigationService = navigationService;
             NavegarNext = new Command(async () => await NavegarNextCommand());
+            SimCommand = new Command(async () => await SimCommandExecute());
+            NaoCommand = new Command(async () => await NaoCommandExecute());
+            SimColor = "#808080";
+            NaoColor = "#808080";
+        }
+
+        private async Task NaoCommandExecute()
+            {
+            NaoColor = "#FF0000";
+            SimColor = "#808080";
+        }
+
+        private async Task SimCommandExecute()
+        {
+            SimColor = "#00FF00";
+            NaoColor = "#808080";
         }
 
         private async Task NavegarNextCommand()
