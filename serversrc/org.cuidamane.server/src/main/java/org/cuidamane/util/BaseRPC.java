5cc732ceef9106ad012bd4d7135b8758a18a5a73
Charlinho <charles.buss@beetools.com.br>
2020-04-05 19:17:07 -0300

    - Criada classe StatusCode e BaseRPC.

---
+++ serversrc/org.cuidamane.server/src/main/java/org/cuidamane/util/BaseRPC.java
@@ -0,0 +1,67 @@
+package org.cuidamane.util;
+
+import io.vertx.core.MultiMap;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
+
+public abstract class BaseRPC {
+
+	private   boolean 		 encodePrettily = false;
+	protected RoutingContext context;
+	protected MultiMap 		 headers;
+	protected String   		 json;
+	
+	protected int responseStatusCode;
+	
+	public BaseRPC setContext(RoutingContext context) {
+		this.context = context;
+		this.headers = context.request().headers();
+		this.json    = context.getBodyAsString("UTF-8");
+		return this;
+	}
+	protected RoutingContext getContext() { return context; }
+	
+	protected String getUrlParam(String key) { return context.request().getParam(key); }
+	
+	protected String getHeader(String key) { return headers.get(key); }
+	
+	public JsonObject getJsonObject() { 
+		return new JsonObject(json); 
+	}
+	
+	protected BaseRPC encodePrettily() {
+		this.encodePrettily = true;
+		return this;
+	}
+	
+	protected BaseRPC setStatusCode(int statusCode) {
+		this.responseStatusCode = statusCode;
+		return this;
+	}
+	
+	public void finish() {
+		this.context.response().setStatusCode(this.responseStatusCode)
+		.putHeader("Content-Type", "application/json")
+		.putHeader("Cache-Control", "private")
+		.end();
+	}
+	
+	public void finishWithError(String error) {
+		JsonObject jsonError = new JsonObject().put("message", error);
+		JsonObject body = new JsonObject().put("error", jsonError);
+		
+		this.context.response().setStatusCode(this.responseStatusCode)
+		.putHeader("Content-Type", "application/json")
+		.putHeader("Cache-Control", "private")
+		.end(encodePrettily ? body.encodePrettily() : body.encode());
+	}
+	
+	public void finish(JsonObject json) {
+		this.context.response().setStatusCode(this.responseStatusCode)
+		.putHeader("Content-Type", "application/json")
+		.putHeader("Cache-Control", "private")
+		.end(encodePrettily ? Json.encodePrettily(json) : Json.encode(json));
+	}
+	
+}
