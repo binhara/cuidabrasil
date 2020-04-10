43cf5315851d541eee37e22d3bc9ee6754d68dc9
carolys <carolina_myasue@hotmail.com>
2020-04-10 18:46:10 -0300

    Teste e classe da requisição das noticias criados.

---
+++ appsrc/AppFVCShared/Model/News.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFVCShared.Model
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Uri { get; set; }
+    }
+}
